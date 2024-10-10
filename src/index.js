39981d6b26ca7085d659b05b937e094512ee4463
Igniter-909 <igniter909505@gmail.com>
2024-10-10 14:55:19 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,36 @@
+// require('dotenv').config({path:'./env'})
+import dotenv from 'dotenv'
+
+import mongoose, { connect } from "mongoose";
+import {DB_NAME} from './constants.js'
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+
+
+
+// import express from "express";
+
+// const app = express()
+
+// ;( async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+//         append.on("error",(error) => {
+//             console.log("ERRR: ",error);
+//             throw error
+//         })
+
+//         app.listen(process.env.PORT,() => {
+//             console.log(`App is listening on ${process.env.PORT}`);
+//         })
+
+//     } catch (error) {
+//         console.log("Error",error)
+//         throw err
+//     }
+// })()
